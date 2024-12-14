4c8c9966fe18309d465ec62e6bb44668401a28c4
br1lisboa <brlisbo@gmail.com>
2024-12-14 16:03:03 -0300

    fix selects

--- src/services/axios-instance.ts
+++ src/services/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = "https://fake-api-listings.vercel.app";
 
 export const axiosInstance = axios.create({
   baseURL,
