e8d95cee1d7fb55713d9dfeccf03ae47744765a0
Bruno Lisboa <58125364+br1lisboa@users.noreply.github.com>
2024-12-14 01:13:20 -0300

    Bruno lisboa (#7)
    * fix some issues add icon red atlas to map
    
    * fix some issues add icon red atlas to map

--- src/hooks/usePutProperty.ts
+++ src/hooks/usePutProperty.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "react-query";
 import { IPostProperty, KEY_PROPERTIES } from "../interfaces";
 import { putProperty } from "../services";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 type UsePutPropertyProps = {
   callback?: () => void;
@@ -9,6 +10,7 @@ type UsePutPropertyProps = {
 
 export function usePutProperty({ callback }: UsePutPropertyProps) {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: ({
@@ -19,11 +21,14 @@ export function usePutProperty({ callback }: UsePutPropertyProps) {
       id: string;
     }) => putProperty(property, id),
     onSuccess: () => {
-      toast.success("Propiedad editada con éxito!");
+      toast.success(
+        "Propiedad editada con éxito! Sera redirigido a la lista de propiedades."
+      );
       queryClient.invalidateQueries(KEY_PROPERTIES);
       if (callback) {
         callback();
       }
+      navigate("/");
     },
     onError: () => {
       toast.error("Ocurrió un error al intentar editar la propiedad!");
