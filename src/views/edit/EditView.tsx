cdc3005fe2961daabfcf09264b6ba6e6b3107723
br1lisboa <brlisbo@gmail.com>
2024-12-14 16:23:06 -0300

    fix README final

--- src/views/edit/EditView.tsx
+++ src/views/edit/EditView.tsx
@@ -13,8 +13,6 @@ export default function EditView() {
     id: params.id || "",
   });
 
-  console.log(data);
-
   return (
     <Layout className="bg-white" isLoading={isLoading || isLoadingEdit}>
       <div className="flex h-full gap-4 w-full my-2">
