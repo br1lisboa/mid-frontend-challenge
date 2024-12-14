ac356e7e0615e1d8a7e1c590fed286652489a28b
br1lisboa <brlisbo@gmail.com>
2024-12-14 01:25:35 -0300

    fix some issues add icon red atlas to map

--- src/views/create/CreateView.tsx
+++ src/views/create/CreateView.tsx
@@ -10,7 +10,11 @@ export default function CreateView() {
     <Layout>
       <div className="flex h-full gap-4 w-full my-2">
         <picture className="hidden sm:block sm:flex-1 m-auto">
-          <Image src="/edificios.jpg" alt="edificios" />
+          <Image
+            src="https://images.unsplash.com/photo-1613310023042-ad79320c00ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="edificios"
+            className="rounded-md shadow-md"
+          />
         </picture>
 
         <section className="flex-1 m-auto">
