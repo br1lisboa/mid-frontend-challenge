f6cd6865d493947d4254245be128add7b86b28b9
br1lisboa <brlisbo@gmail.com>
2024-12-14 15:28:48 -0300

    fix env and added global state

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/properties": {
-        target: process.env.VITE_BASE_URL,
+        target: "https://fake-api-listings.vercel.app",
         changeOrigin: true,
       },
     },
